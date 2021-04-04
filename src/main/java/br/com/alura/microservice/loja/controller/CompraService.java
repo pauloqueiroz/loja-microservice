4e3380033529a78a315d358e90714b313c1ad89f
Paulo Sérgio Queiroz de Oliveira Filho <paulosergio@infoway-pi.com.br>
2021-04-04 17:03:14 -0300

    implementação de método para listagem de instancias do fornedor apenas para visualização das diferentes instâncias que o loadbalancer conhece

--- src/main/java/br/com/alura/microservice/loja/controller/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/controller/CompraService.java
@@ -1,6 +1,7 @@
 package br.com.alura.microservice.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,24 @@ public class CompraService {
 
 	@Autowired
 	private RestTemplate rest;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void realizaCompra(CompraForm compra) {
 		ResponseEntity<InfoFornecedorDto> exchange = rest.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDto.class);
+		
+		imprimeInstanciasLoadBalancer();
+		
 		System.out.println(exchange.getBody().getEndereco());
+	}
+
+	private void imprimeInstanciasLoadBalancer() {
+		eurekaClient.getInstances("fornecedor").stream().forEach(
+				fornecedor->{
+					System.out.println(fornecedor.getHost() + ":"+fornecedor.getPort()+" - "+fornecedor.getServiceId() + " - " +fornecedor.getInstanceId());
+				});
+		
 	}
 
 	
