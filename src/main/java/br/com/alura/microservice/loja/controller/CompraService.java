ca67e6aea96f9fe599654728ea2371f140491304
Paulo Sérgio Queiroz de Oliveira Filho <paulosergio@infoway-pi.com.br>
2021-04-06 13:04:46 -0300

    Inclusão de logs da aplicação e envio dos mesmos para centralização no papertrail

--- src/main/java/br/com/alura/microservice/loja/controller/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/controller/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +14,23 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 
+	static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
 	public Compra realizaCompra(CompraForm compra) {
+		LOG.info("Realizando compra: "+ compra);
 		InfoFornecedorDto infoFornecedor = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		LOG.info("Consultou fornecedor. Fornecedor retornado: "+infoFornecedor.getEndereco());
 		InfoPedidoDto infoPedido = fornecedorClient.realizaPedido(compra.getItens());
-		System.out.println(infoFornecedor.getEndereco());
-		System.out.println(infoPedido);
+		LOG.info("Realizou pedido. Pedido retornado: "+infoPedido.getId());
+
 		
 		Compra compraSalva = new Compra();
 		compraSalva.setPedidoId(infoPedido.getId());
 		compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
 		compraSalva.setEnderecoDestino(infoFornecedor.getEndereco());
+		LOG.info("Salvou compra. "+compra.getEndereco());
 		return compraSalva;
 	}
 
